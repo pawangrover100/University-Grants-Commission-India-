09b7b1e60a9d3b54d61ff04fad9703ae4b277d76
pawan <pawangrover904@gmail.com>
2026-05-29 16:25:18 +0530

    create slider and slider.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
+import Slider from "react-slick";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Slider />>
     </>
   );
 }
