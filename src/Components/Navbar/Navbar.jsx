3f65839d471665c911832432f97570c8e7530138
pawan <pawangrover904@gmail.com>
2026-05-29 15:08:04 +0530

    add tailwind and react icons

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+function Navbar() {
+  return (
+    <>
+    <div className="mx-w-[1400px] bg-[#3d344b] h-[5vh] box-border border-2 border-solid ">
+
+    </div>
+    </>
+  )
+}
+
+export default Navbar
