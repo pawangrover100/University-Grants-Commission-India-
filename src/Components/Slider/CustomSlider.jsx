3209c53ad1957e57032d419e3cea18fbdf649c20
pawan <pawangrover904@gmail.com>
2026-06-01 13:44:46 +0530

    add CustomSlider Carousel

--- src/Components/Slider/CustomSlider.jsx
+++ src/Components/Slider/CustomSlider.jsx
@@ -1,8 +1,17 @@
 import React from "react";
-import SliderComponent from "react-slick";
+// Import the component with a different name to avoid the declaration error
+import SlickSlider from "react-slick";
 
-// Vite/React 19 की समस्या को ठीक करने के लिए
-const Slider = SliderComponent.default || SliderComponent;
+// Essential CSS for the slider
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import video_slider from "../../assets/video_slider.mp4";
+import main_slider2 from "../../assets/main-slider2.png";
+import main_slider3 from "../../assets/main-slider3.png";
+
+// Vite/React 19 compatibility fix using the renamed import
+const Slider = SlickSlider.default || SlickSlider;
 
 function CustomSlider() {
   const settings = {
@@ -11,24 +20,46 @@ function CustomSlider() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    arrows: true,
   };
 
   return (
-    <div className="slider-container" style={{ width: "100%", margin: "0 auto", padding: "40px" }}>
+    <div className="slider-container" style={{ width: "100%", overflow: "hidden" }}>
       <Slider {...settings}>
-        <div>
-          <div style={{ background: "#5f9ea0", padding: "50px", textAlign: "center", color: "#fff" }}>
-            <h3>1</h3>
+        {/* Slide 1: Video */}
+        <div className="outline-none">
+          <div style={{ background: "#000", height: "500px", width: "100%" }}>
+            <video 
+              src={video_slider} 
+              style={{ width: "100%", height: "100%", objectFit: "cover" }} 
+              autoPlay 
+              muted 
+              loop 
+              playsInline
+            />
           </div>
         </div>
-        <div>
-          <div style={{ background: "#5f9ea0", padding: "50px", textAlign: "center", color: "#fff" }}>
-            <h3>2</h3>
+
+        {/* Slide 2: Image */}
+        <div className="outline-none">
+          <div style={{ background: "#5f9ea0", height: "500px", width: "100%" }}>
+            <img 
+              src={main_slider2} 
+              alt="Slide 2" 
+              style={{ width: "100%", height: "100%", objectFit: "cover" }} 
+            />
           </div>
         </div>
-        <div>
-          <div style={{ background: "#5f9ea0", padding: "50px", textAlign: "center", color: "#fff" }}>
-            <h3>3</h3>
+
+        {/* Slide 3: Image */}
+        <div className="outline-none">
+          <div style={{ background: "#5f9ea0", height: "500px", width: "100%" }}>
+            <img 
+              src={main_slider3} 
+              alt="Slide 3" 
+              style={{ width: "100%", height: "100%", objectFit: "cover" }} 
+            />
           </div>
         </div>
       </Slider>
