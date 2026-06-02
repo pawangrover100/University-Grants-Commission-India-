ba08bb2bf3a00b0b8050685d5ad5394d8ab719cc
pawan <pawangrover904@gmail.com>
2026-06-02 12:33:43 +0530

    make add section

--- src/Components/AddSection/AddSection.jsx
+++ src/Components/AddSection/AddSection.jsx
@@ -7,13 +7,13 @@ function AddSection() {
       id: 1,
       image: add,
       title: "About University Grants Commission",
-      para: "The University Grants Commission (UGC) came into existence on 28th December, 1953 and became a statutory Organization of the Government of India by an Act of Parliament in 1956, for the coordination, determination and maintenance of standards of teaching, examination and research in university education.",
+      paraline: `The University Grants Commission (UGC) came into existence on 28th December, 1953 and became a statutory Organization of the Government of India by an Act of Parliament in 1956, for the coordination, determination and maintenance of standards of teaching, examination and research in university education.`,
     },
   ];
 
   const renderinfo = info.map((item) => {
     return (
-      <div key={item.id} className="w-full flex flex-col my-10 items-center">
+      <div key={item.id} className="w-full flex flex-col my-5 items-center">
         {/* Image Container */}
         <div className="flex justify-center w-full px-4">
           <img
@@ -23,29 +23,22 @@ function AddSection() {
           />
         </div>
 
-       
-        <div className="bg-white shadow-2xl  z-10 p-8 max-w-full">
-          <div className="text-left">
-            <h3 className="text-2xl md:text-3xl font-bold ml-40 text-orange-500 mb-6">
-              {item.title}
-            </h3>
-           <div className=" ml-40">
-              <p className="text-base text-zinc-600 leading-relaxed text-justify">
-              {item.para}
-            </p>
-           </div>
-          
+        <div className="bg-white shadow-lg z-10 p-8 h-[300px] border-2 my-2 border-red-500 w-[1470px]">
+          <h2 className="text-3xl font-bold ml-40">
+            <span className="text-orange-500">About</span>{" "}
+            {item.title.replace("About ", "")}
+          </h2>
+          <div className=" h-fit my-5 border-2 ml-40 border-red-500 w-[1090px]">
+        <p>{item.paraline}</p>
           </div>
+          
         </div>
+        
       </div>
     );
   });
 
-  return (
-    <section className="w-full h-fit mt-5">
-      {renderinfo}
-    </section>
-  );
+  return <section className="w-full h-fit mt-5">{renderinfo}</section>;
 }
 
 export default AddSection;
